ad4368a71c03c23ec7942a0a3db43d717cf3624b
Akshay Vijay Jain <theakshay.jain@gmail.com>
2018-12-11 15:41:40 +0530

    columns with data added

--- js/index.js
+++ js/index.js
@@ -1,20 +1,51 @@
+const makeSchoolTweetUrl = 'http://localhost:7890/1.1/statuses/user_timeline.json?count=30&screen_name=makeschool';
+const newsYComTweetUrl = 'http://localhost:7890/1.1/statuses/user_timeline.json?count=30&screen_name=newsycombinator';
+const yComTweetUrl = 'http://localhost:7890/1.1/statuses/user_timeline.json?count=30&screen_name=ycombinator';
+const selfTweetUrl = 'http://localhost:7890/1.1/statuses/user_timeline.json?count=30';
 
-function onLoadFunction() {
-  console.log('hi');
-  fetch('http://localhost:7890/1.1/statuses/user_timeline.json?count=30&screen_name=makeschool').then(res=>res.json()).then(tweets=> {
-    console.log(tweets);
-    renderTweets(tweets);
+const userAccounts = [
+  {
+    columnName: document.getElementById('makeSchoolCol'),
+    tweetUrl: makeSchoolTweetUrl,
+    visible: true,
+  },
+  {
+    columnName: document.getElementById('newsYComCol'),
+    tweetUrl: newsYComTweetUrl,
+    visible: true,
+  },
+  {
+    columnName: document.getElementById('yComCol'),
+    tweetUrl: yComTweetUrl,
+    visible: true,
+  },
+  {
+    columnName: document.getElementById('selfCol'),
+    tweetUrl: selfTweetUrl,
+    visible: false,
+  }
+]
+function loadTweets() {
+  console.log('inside window.onload function');
+  userAccounts.filter(account=>account.visible).forEach(account => {
+    getTweets(account.tweetUrl, account.columnName);
   });
 }
-window.onload = onLoadFunction;
+function getTweets(url, columnName) {
+  fetch(url).then(res=>res.json()).then(tweets=> {
+    console.log(tweets);
+    renderTweets(tweets, columnName);
+  });
+}
+window.onload = loadTweets;
 
-function renderTweets(tweets){
+function renderTweets(tweets, columnName){
   const tweetHtmlArray = tweets.map(tweet => {
-    return `<div class="userName">${tweet.user.name}</div><div class="userTweet">${tweet.text}</div><br /><br />`;
+    return `<div class="userName">${tweet.user.name}</div><div class="userTweet">${tweet.text}</div><div class="create_at_date">${tweet.user.created_at}</div><a href="https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}">Link to tweet</a><br /><br />`;
   });
   const tweetHTML = tweetHtmlArray.join(' ')
-  // return tweetHTML;
   console.log(tweetHTML);
-  document.getElementsByClassName('twit-col-1')[0].innerHTML = tweetHTML;
-  // return `<div>t`;
+  if(columnName){
+    columnName.innerHTML = tweetHTML;
+  }
 }
